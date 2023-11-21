52f1be9794638d5f7402a3d714e2d5f82c1704e7
guan <guan@localhost.localdomain>
2023-11-21 23:26:11 +0100

    Nästan Vue klar

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,20 +1,37 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
-  </template>
+  <div>
+    <h2>{{ burger.name }}</h2>
+    <img v-bind:src="burger.img" style="width: 200px;">
+    <ul class="allergies">
+      <li>{{ burger.kCal }} kcal</li>
+      <li v-if="burger.gluten">
+        Gluten
+      </li>
+      <li v-if="burger.lactose">
+        Lactose
+      </li>
+    </ul>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'OneBurger',
-    props: {
-      burger: Object
+<script>
+export default {
+  name: 'OneBurger',
+  props: {
+    burger: Object
+  },
+  data: function () {
+    return {
+      amountOrdered: 0
     }
   }
-  </script>
+}
+
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  
-  </style>
-  
+<style scoped>
+.allergies {
+  font-weight: bold;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,100 @@
 <template>
-  <div>
-    <div>
-      <h1>Burgers</h1>
-      <Burger v-for="burger in burgers"
-              v-bind:burger="burger" 
-              v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-      click here
-    </div>
-  </div>
+  <body>
+    <header>
+      <h1 style="position:absolute; color:white; top: 50%; left: 50%; transform: translate(-50%, -50%);">Welcome to
+        Bird Burgers!</h1>
+      <img src="img/burger_header.jpeg" id="burger_header">
+    </header>
+
+    <section id="burgers">
+      <h2>We make the only bird burgers in town!</h2>
+      <p>Choose bird borger:</p>
+
+      <div class="wrapper">
+        <Burger v-for="burger in burgers" v-bind:burger="burger" v-bind:key="burger.name" />
+      </div>
+    </section>
+
+    <section id="order_information">
+      <h3>Who are you?</h3>
+      <p>Provide information for delivery:</p>
+
+      <form>
+        <p>
+          <label for="fullname">Full name</label><br>
+          <input type="text" id="fullname" v-model="fn" required="required" placeholder="First- and Last name">
+        </p>
+        <p>
+          <label for="email">E-mail</label><br>
+          <input type="text" id="email" v-model="em" placeholder="E-mail address">
+        </p>
+        <p>
+          <label for="street">Street</label><br>
+          <input type="text" id="street" v-model="st" required="required" placeholder="Street name">
+        </p>
+        <p>
+          <label for="house">House number</label><br>
+          <input type="text" id="housenumber" v-model="hn" required="required" placeholder="House number">
+        </p>
+
+        <p>
+          <label for="payment_method">Payment method</label><br>
+          <select id="payment_method" v-model="pm">
+            <option>Card</option>
+            <option>Swish</option>
+            <option>Monero</option>
+            <option selected="selected">OSRS gp</option>
+          </select>
+        </p>
+
+        <p>
+          <label for="male">Male</label>
+          <input type="radio" id="gender" v-model="gender" value="male">
+
+          <label for="female">Female</label>
+          <input type="radio" id="gender" v-model="gender" value="female">
+
+          <label for="other">Other</label>
+          <input checked type="radio" id="gender" v-model="gender" value="other">
+        </p>
+      </form>
+
+      <button v-on:click="orderButtonClick">
+        <img src="img/submit-button-png-25802.png" style="width: 100px">
+      </button>
+    </section>
+
+    <hr>
+    <footer>
+      <div class="footer-content">
+        <p>&copy; Bird Burger Company</p>
+        <p>Contact us at <a href="mailto:contact@fakeburger.com">contact@birdburger.com</a></p>
+      </div>
+    </footer>
+
+  </body>
 </template>
 
 <script>
 import Burger from '../components/OneBurger.vue'
 import io from 'socket.io-client'
+import menu from '../assets/menu.json'
 
 const socket = io();
+const myBurgers = menu; // menu is imported from menu.json
+
+/* function MenuItem(name, kCal, gluten, lactose, img) {
+  this.name = name;
+  this.kCal = kCal;
+  this.gluten = gluten;
+  this.lactose = lactose;
+  this.img = img;
+} */
+
+/* const myBurgers = [
+  new MenuItem('Borb burger', 800, true, true, '../../img/burger1.jpeg'),
+  new MenuItem('Crazy burger', 2500, true, true, '../../img/crazy_borger.png'),
+  new MenuItem('Mmmm burger', 500, true, false, '../../img/sus_borger.png')] */
 
 export default {
   name: 'HomeView',
@@ -25,34 +103,97 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: myBurgers,
+      /*       fn: "",
+            em: "",
+            st: "",
+            hn: "", */
+      pm: "OSRS gp",
+      gender: "Other"
     }
   },
   methods: {
     getOrderNumber: function () {
-      return Math.floor(Math.random()*100000);
+      return Math.floor(Math.random() * 100000);
+    },
+    orderButtonClick() {
+      console.log(
+        "Order button clicked",
+        this.fn,
+        this.em,
+        this.st,
+        this.hn,
+        this.pm,
+        this.gender
+      )
     },
     addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
-      socket.emit("addOrder", { orderId: this.getOrderNumber(),
-                                details: { x: event.clientX - 10 - offset.x,
-                                           y: event.clientY - 10 - offset.y },
-                                orderItems: ["Beans", "Curry"]
-                              }
-                 );
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
+      socket.emit("addOrder", {
+        orderId: this.getOrderNumber(),
+        details: {
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y
+        },
+        orderItems: ["Beans", "Curry"]
+      }
+      );
     }
   }
 }
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+body {
+  font-family: Arial;
+  font-size: 1.2em;
+}
+
+#burgers {
+  background-color: black;
+  color: white;
+  border: white 2px dotted;
+}
+
+button:hover {
+  background-color: lawngreen;
+  color: white;
+  cursor: pointer;
+}
+
+section {
+  margin: 20px;
+  border: black 2px dotted;
+  padding: 20px;
+}
+
+button {
+  margin: 10px;
+}
+
+header {
+  position: relative;
+  margin: 20px;
+  height: 300px;
+  overflow: hidden;
+}
+
+#burger_header {
+  opacity: 0.;
+  width: auto;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+#map {
+  width: 300px;
+  height: 300px;
+  background-color: red;
+}
 </style>
